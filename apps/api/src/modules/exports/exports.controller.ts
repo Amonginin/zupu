f6d01b26cb725e0a36b431f315eb953908f32b54
Amonginin <1761966464@qq.com>
2026-04-02 23:42:56 +0800

    fix(api): 修复导出文件下载链路

--- apps/api/src/modules/exports/exports.controller.ts
+++ apps/api/src/modules/exports/exports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Headers, Param, Post } from '@nestjs/common';
+import { Controller, Get, Headers, Param, Post, StreamableFile, Header } from '@nestjs/common';
 import { ExportsService } from './exports.service';
 
 @Controller('exports')
@@ -13,5 +13,15 @@ export class ExportsController {
   @Get(':id')
   get(@Headers('x-family-id') familyId = 'demo-family', @Param('id') id: string) {
     return this.exportsService.getTask(familyId, id);
+  }
+
+  @Get(':id/download')
+  @Header('Content-Disposition', 'attachment')
+  async download(@Headers('x-family-id') familyId = 'demo-family', @Param('id') id: string) {
+    const file = await this.exportsService.downloadTask(familyId, id);
+    return new StreamableFile(file.buffer, {
+      type: file.mimeType,
+      disposition: `attachment; filename="${file.fileName}"`,
+    });
   }
 }
